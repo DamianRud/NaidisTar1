e072b4c184799b363b2a8b12267ed3f0f9c9004a
opilane <opilane@HP-CZC2349HTF>
2026-02-05 12:21:44 +0200

    Add project files.

---
+++ NaidisTar1/NaidisFunktsionid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaidisTar1
+{
+    public class NaidisFunktsionid
+    {
+        public static float Summa(float arv1,float arv2)
+        {
+            return arv1 + arv2;
+        }
+    }
+}
---
+++ NaidisTar1/StartPage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Channels;
+
+namespace NaidisTar1
+{
+    public class StartPage
+    {
+        public static void Main(string[]args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("1.Osa andmetüübid,Alamfunktsionid/meetodid");
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            string text = "Tere Tulemast C-i maailma";
+            Console.WriteLine();
+            Console.WriteLine("Oli loodud muutuja tekst, mis võrdub", text);
+            Console.WriteLine();
+            Console.Write("Mis on sinu nimi:");
+            string nimi = Console.ReadLine();
+            Console.WriteLine("kui vana sa oled!:");
+            try
+            { 
+                int vanus = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Tere{nimi}.Sa oled {vanus} aasta vana");
+            }
+            catch (Exception e)
+            { 
+              
+                    Console.WriteLine(e);
+            }
+            Console.WriteLine("Arv1:");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Arv2:");
+            float b = float.Parse(Console.ReadLine());
+            float vastus = NaidisFunktsionid.Summa(a, b);
+            Console.WriteLine($"Summa {a} ja {b} võrdub {vastus}");
+
+            
+
+
+
+
+        }
+
+    }
+}
